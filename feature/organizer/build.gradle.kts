b81536af7c66f8694435dcfe9ae66b659d039af9
Samuel-1111 <olanlokunajibola2011@gmail.com>
2026-09-18 23:33:35 +0100

    fix(build): configure KSP for Hilt feature modules

--- feature/organizer/build.gradle.kts
+++ feature/organizer/build.gradle.kts
@@ -1,4 +1,4 @@
-plugins { alias(libs.plugins.android.library); alias(libs.plugins.kotlin.android); alias(libs.plugins.kotlin.compose); alias(libs.plugins.hilt) }
+plugins { alias(libs.plugins.android.library); alias(libs.plugins.kotlin.android); alias(libs.plugins.kotlin.compose); alias(libs.plugins.ksp); alias(libs.plugins.hilt) }
 android { namespace = "com.nexa.feature.organizer"; compileSdk = 35; defaultConfig { minSdk = 26 }; buildFeatures { compose = true } }
 dependencies {
     implementation(project(":domain"))
