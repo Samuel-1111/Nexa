311fa52024f663bfcd8dd33960591a8c28cb7539
Samuel-1111 <olanlokunajibola2011@gmail.com>
2026-09-20 09:49:29 +0100

    Provide safe singleton access to NEXA Room database for boot recovery

--- core/database/src/main/kotlin/com/nexa/core/database/NexaDatabase.kt
+++ core/database/src/main/kotlin/com/nexa/core/database/NexaDatabase.kt
@@ -2,6 +2,8 @@ package com.nexa.core.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import android.content.Context
+import androidx.room.Room
 
 @Database(
     entities = [TaskEntity::class, ReminderEntity::class, NoteEntity::class, MemoryEntity::class, OutboxOperationEntity::class],
@@ -14,4 +16,17 @@ abstract class NexaDatabase : RoomDatabase() {
     abstract fun noteDao(): NoteDao
     abstract fun memoryDao(): MemoryDao
     abstract fun outboxDao(): OutboxDao
+
+    companion object {
+        @Volatile private var INSTANCE: NexaDatabase? = null
+
+        fun getInstance(context: Context): NexaDatabase =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    NexaDatabase::class.java,
+                    "nexa.db",
+                ).build().also { INSTANCE = it }
+            }
+    }
 }
