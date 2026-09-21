5e30e281029c2493784518cf9b72ca9d713dc758
Samuel-1111 <olanlokunajibola2011@gmail.com>
2026-09-21 11:12:23 +0100

    Fix Kotlin import formatting

--- app/src/main/kotlin/com/nexa/app/navigation/NexaApp.kt
+++ app/src/main/kotlin/com/nexa/app/navigation/NexaApp.kt
@@ -17,7 +17,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp\nimport androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
