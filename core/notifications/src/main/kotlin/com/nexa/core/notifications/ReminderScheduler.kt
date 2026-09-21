179d876bf770937a4fb80c590972d6bb616ec74c
Samuel-1111 <olanlokunajibola2011@gmail.com>
2026-09-21 02:25:49 +0100

    Use exact reminder alarms when permitted

--- core/notifications/src/main/kotlin/com/nexa/core/notifications/ReminderScheduler.kt
+++ core/notifications/src/main/kotlin/com/nexa/core/notifications/ReminderScheduler.kt
@@ -19,7 +19,11 @@ class AlarmManagerReminderScheduler(private val context: Context) : ReminderSche
             .putExtra(ReminderAlarmReceiver.EXTRA_REMINDER_ID, reminder.id.value)
             .putExtra(ReminderAlarmReceiver.EXTRA_REMINDER_TITLE, reminder.title)
         val pendingIntent = PendingIntent.getBroadcast(context, reminder.id.value.hashCode(), intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
-        alarmManager.setAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, reminder.triggerAt.toEpochMilli(), pendingIntent)
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S && alarmManager.canScheduleExactAlarms()) {
+            alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, reminder.triggerAt.toEpochMilli(), pendingIntent)
+        } else {
+            alarmManager.setAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, reminder.triggerAt.toEpochMilli(), pendingIntent)
+        }
     }
 
     override fun cancel(reminder: Reminder) {
