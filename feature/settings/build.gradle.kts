0c657b5602fab4388503d38e49ed37d2bce06b2a
Samuel-1111 <olanlokunajibola2011@gmail.com>
2026-09-21 02:27:07 +0100

    Connect Settings to profile and subscription data

--- feature/settings/build.gradle.kts
+++ feature/settings/build.gradle.kts
@@ -2,6 +2,9 @@ plugins { alias(libs.plugins.android.library); alias(libs.plugins.kotlin.android
 android { namespace = "com.nexa.feature.settings"; compileSdk = 35; defaultConfig { minSdk = 26 }; buildFeatures { compose = true } }
 dependencies {
     implementation(project(":core:designsystem"))
+    implementation(project(":core:network"))
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
     implementation(platform("androidx.compose:compose-bom:2024.12.01"))
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
