e9142be754a64efc0e7bb47bbd76aa1d77a3bf02
Samuel-1111 <olanlokunajibola2011@gmail.com>
2026-09-21 11:11:08 +0100

    Restore exact reminder alarms after reboot when permitted

--- core/notifications/src/main/kotlin/com/nexa/core/notifications/BootReceiver.kt
+++ core/notifications/src/main/kotlin/com/nexa/core/notifications/BootReceiver.kt
@@ -28,7 +28,7 @@ class BootReceiver : BroadcastReceiver() {
                             alarmIntent,
                             android.app.PendingIntent.FLAG_UPDATE_CURRENT or android.app.PendingIntent.FLAG_IMMUTABLE,
                         )
-                        alarms.setAndAllowWhileIdle(android.app.AlarmManager.RTC_WAKEUP, row.triggerAtEpochMs, pi)
+                        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S && alarms.canScheduleExactAlarms()) {\n                            alarms.setExactAndAllowWhileIdle(android.app.AlarmManager.RTC_WAKEUP, row.triggerAtEpochMs, pi)\n                        } else {\n                            alarms.setAndAllowWhileIdle(android.app.AlarmManager.RTC_WAKEUP, row.triggerAtEpochMs, pi)\n                        }
                     }
                 }
             } finally {
